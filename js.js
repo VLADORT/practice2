01dfee5eacd431c9697103433cafe1728e14f1ce
VLADORT <vlad300820001@gmail.com>
2018-12-09 22:10:38 +0200

    Upload

---
+++ js.js
@@ -0,0 +1,8 @@
+let block = document.querySelector('.block__item');
+block.addEventListener('click', function() {
+    block.style.width = "400px";
+    block.style.height = "30vh";
+    block.style.background = "rgb(32, 224, 7)";
+
+
+});
